58b64024fec0cbe9aad3711f3cb0aeb53100dd11
Ahmedjabnouni <116764514+Ahmedjabnouni@users.noreply.github.com>
2024-05-14 01:40:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
     const grid = document.getElementById('svgGrid');
     const searchInput = document.querySelector('.input');
-    let fuse; // Fuse.js instance
-    let svgsData = []; // Store SVG data
+    const colorPicker = document.getElementById('colorPicker');
+    let fuse; // Declare a variable to store your Fuse instance
+    let svgsData = []; // Array to hold the SVG data
 
-    fetch('svgs.json')
+    fetch('svgs.json')  // Fetch the SVG data
         .then(response => response.json())
         .then(svgs => {
-            svgsData = svgs;
+            svgsData = svgs; // Store the fetched SVG data globally
             const options = {
                 isCaseSensitive: false,
                 includeScore: true,
@@ -20,61 +21,19 @@ document.addEventListener('DOMContentLoaded', function () {
                 distance: 100,
                 keys: ["name", "tags"]
             };
-            fuse = new Fuse(svgs, options);
-            updateGrid('');
+            fuse = new Fuse(svgs, options); // Initialize Fuse.js with the fetched SVG data
+            updateGrid(''); // Display all SVGs initially
         });
 
-    const pickr = Pickr.create({
-        el: '#colorPicker',
-        theme: 'nano', // nano theme
-        default: '#0015FF',
-        swatches: [
-            'rgba(244, 67, 54, 1)',
-            'rgba(233, 30, 99, 0.95)',
-            'rgba(156, 39, 176, 0.9)',
-            'rgba(103, 58, 183, 0.85)',
-            'rgba(63, 81, 181, 0.8)',
-            'rgba(33, 150, 243, 0.75)',
-            'rgba(3, 169, 244, 0.7)',
-            'rgba(0, 188, 212, 0.7)',
-            'rgba(0, 150, 136, 0.75)',
-            'rgba(76, 175, 80, 0.8)',
-            'rgba(139, 195, 74, 0.85)',
-            'rgba(205, 220, 57, 0.9)',
-            'rgba(255, 235, 59, 0.95)',
-            'rgba(255, 193, 7, 1)'
-        ],
-        components: {
-            preview: true,
-            opacity: true,
-            hue: true,
-
-            interaction: {
-                hex: true,
-                rgba: false,
-                hsla: false,
-                hsva: false,
-                cmyk: false,
-                input: true,
-                clear: false,
-                save: true
-            }
-        }
-    });
-
-   
-    pickr.on('change', (color, source, instance) => {
-        const newColor = color.toRGBA().toString(0);
-        updateSVGColors(newColor);
-    });
-
     function updateGrid(term) {
-        const results = term ? fuse.search(term) : svgsData;
-        grid.innerHTML = '';
+        const results = term ? fuse.search(term) : svgsData; // Use Fuse.js to search for the term or default to showing all items
+        grid.innerHTML = ''; // Clear the grid before displaying new results
+
         results.forEach(result => {
             const svg = result.item ? result.item : result;
             const col = document.createElement('div');
             col.className = 'col-sm-6 col-md-4 col-lg-3 mb-4';
+
             const card = document.createElement('div');
             card.className = 'card h-100 position-relative';
             card.onclick = () => openInEditor(card);
@@ -85,8 +44,7 @@ document.addEventListener('DOMContentLoaded', function () {
             downloadIcon.setAttribute('width', '24');
             downloadIcon.setAttribute('height', '24');
             downloadIcon.setAttribute('class', 'download-icon');
-            downloadIcon.innerHTML = '<path d="M17 17H17.01M17.4 14H18C18.9319 14 19.3978 14 19.7654 14.1522C20.2554 14.3552 20.6448 14.7446 20.8478 15.2346C21 15.6022 21 16.0681 21 17C21 17.9319 21 18.3978 20.8478 18.7654C20.6448 19.2554 20.2554 19.6448 19.7654 19.8478C19.3978 20 18.9319 20 18 20H6C5.06812 20 4.60218 20 4.23463 19.8478C3.74458 19.6448 3.35523 19.2554 3.15224 18.7654C3 18.3978 3 17.9319 3 17C3 16.0681 3 15.6022 3.15224 15.2346C3.35523 14.7446 3.74458 14.3552 4.23463 14.1522C4.60218 14 5.06812 14 6 14H6.6M12 15V4M12 15L9 12M12 15L15 12" stroke="#CBD5E1" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>';
-            card.appendChild(downloadIcon);
+            downloadIcon.innerHTML = `<path d="M17 17H17.01M17.4 14H18C18.9319 14 19.3978 14 19.7654 14.1522C20.2554 14.3552 20.6448 14.7446 20.8478 15.2346C21 15.6022 21 16.0681 21 17C21 17.9319 21 18.3978 20.8478 18.7654C20.6448 19.2554 20.2554 19.6448 19.7654 19.8478C19.3978 20 18.9319 20 18 20H6C5.06812 20 4.60218 20 4.23463 19.8478C3.74458 19.6448 3.35523 19.2554 3.15224 18.7654C3 18.3978 3 17.9319 3 17C3 16.0681 3 15.6022 3.15224 15.2346C3.35523 14.7446 3.74458 14.3552 4.23463 14.1522C4.60218 14 5.06812 14 6 14H6.6M12 15V4M12 15L9 12M12 15L15 12" stroke="#CBD5E1" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>`;
 
             fetch(svg.url)
                 .then(response => response.text())
@@ -95,6 +53,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     cardBody.className = 'card-body d-flex flex-column';
                     cardBody.innerHTML = `<h5 class="card-title">${svg.name}</h5><div class="svg-container flex-grow-1">${svgData}</div><p class="card-text">${svg.tags.join(', ')}</p>`;
                     card.appendChild(cardBody);
+                    card.appendChild(downloadIcon);
                     col.appendChild(card);
                     grid.appendChild(col);
                     markInitialElements(card.querySelector('.svg-container'), '#6c63ff');
@@ -106,12 +65,17 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
-    function openInEditor(svgCard) {
-        const svgData = svgCard.querySelector('.svg-container').innerHTML;
-        localStorage.setItem('currentSVG', svgData); // Store SVG data in local storage
-        window.location.href = 'editor-index.html'; // Navigate to the editor page
-    }
+    // Event listeners
+    searchInput.addEventListener('input', function () {
+        updateGrid(searchInput.value);
+    });
 
+    colorPicker.addEventListener('input', function (e) {
+        const newColor = e.target.value;
+        updateSVGColors(newColor);
+    });
+
+    // Color update functions
     function markInitialElements(svgContainer, targetColor) {
         const elements = svgContainer.querySelectorAll('path, circle, rect, polygon, polyline, line, ellipse');
         elements.forEach(element => {
@@ -128,20 +92,7 @@ document.addEventListener('DOMContentLoaded', function () {
             element.setAttribute('fill', newColor);
         });
     }
-
-    function downloadSVG(svgData, name) {
-        const blob = new Blob([svgData], {type: 'image/svg+xml'});
-        const url = window.URL.createObjectURL(blob);
-        const a = document.createElement('a');
-        a.href = url;
-        a.download = name + '.svg';
-        document.body.appendChild(a);
-        a.click();
-        document.body.removeChild(a);
-        window.URL.revokeObjectURL(url);
-    }
 });
-
 
 function openInEditor(svgCard) {
     const svgData = svgCard.querySelector('.svg-container').innerHTML;
@@ -185,6 +136,3 @@ function downloadSVG(svgData, name) {
   document.body.removeChild(a); // Remove the anchor after download.
   window.URL.revokeObjectURL(url); // Clean up the blob URL.
 }
-
-
-
